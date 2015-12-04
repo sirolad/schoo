379f7438d6800a27aa3ceaa37dafa312ee1d672c
andela-sakande <surajudeen.akande@andela.com>
2015-12-04 09:31:07 +0100

    Add Material css to public

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Laravel</title>
+        <title>Schoo</title>
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">Schoo</div>
             </div>
         </div>
     </body>
