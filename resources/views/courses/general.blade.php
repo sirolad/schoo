7e7a57ab20ab5a0a2f2e8118b5edb6cc8f922394
andela-sakande <surajudeen.akande@andela.com>
2015-12-14 23:03:40 +0000

    Add Section views functionality

--- resources/views/courses/general.blade.php
+++ resources/views/courses/general.blade.php
@@ -7,4 +7,31 @@
     <h1>General Knowledge</h1>
     </div>
 </div>
+<div class="container" style="min-height: 180px">
+    <ul class="breadcrumb">
+      <li><a href="/">Home</a></li>
+      <li class="active">General Knowledge</li>
+    </ul>
+
+    <div class="row previews">
+    @if($courses)
+        @foreach($courses as $course)
+            <div class="col-lg-3 col-md-6 col-sm-6">
+            <div class="thumbnail">
+                <a href="/courses/{{ $course->id }}" class="post-image-link">
+                    <p>
+                        <img src="http://i1.ytimg.com/vi/{{ $course->video_id }}/hqdefault.jpg" class="img-responsive" alt="Course Image">
+                    </p>
+                </a>
+                <div class="caption">
+                    <h3>{{ $course->course}}</h3>
+                    <p>{{ $course->description }}</p>
+                    <a href="/courses/{{ $course->id }}" class="btn btn-primary btn-raised">Start Course</a>
+                </div>
+            </div>
+        </div>
+        @endforeach
+    @endif
+  </div>
+</div>
 @endsection
