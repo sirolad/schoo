22aff3d715d5903fe8515cd6df26d49a022188d1
andela-sakande <surajudeen.akande@andela.com>
2015-12-14 22:47:45 +0000

    Outline Section Pages

--- resources/views/courses/computer.blade.php
+++ resources/views/courses/computer.blade.php
@@ -1,1 +1,10 @@
 @extends('layouts.master')
+@section('title','Computer Course')
+
+@section('content')
+<div class="jumbotron move2">
+    <div class="container">
+    <h1>Computer Science</h1>
+    </div>
+</div>
+@endsection
--- resources/views/courses/languages.blade.php
+++ resources/views/courses/languages.blade.php
@@ -1,1 +1,10 @@
 @extends('layouts.master')
+@section('title','Computer Course')
+
+@section('content')
+<div class="jumbotron move2">
+    <div class="container">
+    <h1>Languages</h1>
+    </div>
+</div>
+@endsection
