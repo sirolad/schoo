8ae9f4c5bf5f8a04f950d61a6899c90358311e09
andela-sakande <surajudeen.akande@andela.com>
2015-12-17 10:43:56 +0000

    Add favicon

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,23 +6,23 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="_token" content="{{ csrf_token() }}" />
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/favicon-32x32.png') }}">
     <!-- Material Design fonts -->
-  <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:300,400,500,700" type="text/css">
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:300,400,500,700" type="text/css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
-  <!-- Bootstrap -->
-  <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap -->
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
 
-  <!-- Bootstrap Material Design -->
-  <link href="{{ asset('css/bootstrap-material-design.css') }}" rel="stylesheet">
-  <link href="{{ asset('css/ripples.min.css') }}" rel="stylesheet">
-  <link href="{{ asset('css/bootstrap-social.css') }}" rel="stylesheet">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-  <link rel="stylesheet" type="text/css" href="{{asset('css/sweetalert.css')}}">
-  <link href="{{ asset('css/simple-sidebar.css') }}" rel="stylesheet">
-  <link href="{{ asset('css/style.css') }}" rel="stylesheet">
-  @yield('custom-css')
+    <!-- Bootstrap Material Design -->
+    <link href="{{ asset('css/bootstrap-material-design.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/ripples.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/bootstrap-social.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="{{asset('css/sweetalert.css')}}">
+    <link href="{{ asset('css/simple-sidebar.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    @yield('custom-css')
 </head>
 <body>
 
